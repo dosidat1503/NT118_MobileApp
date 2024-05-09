27d91c18fa35928c44d97fea74fc3fac54d643a1
lexuan2602 <vongoclexuan@gmail.com>
2024-05-10 02:35:38 +0700

    aaaaa

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,40 +1,44 @@
-import { View, Text, StyleSheet, TextInput, ImageBackground, TouchableOpacity } from 'react-native';
-import React, { useEffect, useState } from 'react';
-import Button from '../components/Button';
-import { Link, Stack, useNavigation } from 'expo-router';
-import { useCartContext } from '@/providers.tsx/CartProvider'; 
-import { CheckBox } from 'react-native-elements';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { FontAwesome5 } from '@expo/vector-icons';
-import axios from 'axios'; 
-
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import Button from "../components/Button";
+import { Link, Stack, useNavigation } from "expo-router";
+import { useCartContext } from "@/providers.tsx/CartProvider";
+import { CheckBox } from "react-native-elements";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { FontAwesome5 } from "@expo/vector-icons";
+import axios from "axios";
 
 const index = () => {
-
-  const {heightScreen, widthScreen, mainColor, baseURL} = useCartContext();
+  const { heightScreen, widthScreen, mainColor, baseURL } = useCartContext();
   const styles = StyleSheet.create({
-      divContainer: {
-          width: widthScreen,
-          height: heightScreen,
-          justifyContent: "center",
-          alignItems: "center",
-          // backgroundImage: "url('https://www.w3schools.com/css/img_lights.jpg')"
-      },
-      input: {
-          width: '90%',
-          height: heightScreen * 0.06,
-          borderRadius: widthScreen * 0.02,
-          borderColor: 'gray',
-          borderWidth: 1,
-          marginTop: 10,
-          paddingHorizontal: 10,
-          backgroundColor: 'white'
-      },
-      
-  })
-  const [signInInfo, setSignInInfo] = useState({ 
-    email: '', 
-    password: '', 
+    divContainer: {
+      width: widthScreen,
+      height: heightScreen,
+      justifyContent: "center",
+      alignItems: "center",
+      // backgroundImage: "url('https://www.w3schools.com/css/img_lights.jpg')"
+    },
+    input: {
+      width: "90%",
+      height: heightScreen * 0.06,
+      borderRadius: widthScreen * 0.02,
+      borderColor: "gray",
+      borderWidth: 1,
+      marginTop: 10,
+      paddingHorizontal: 10,
+      backgroundColor: "white",
+    },
+  });
+  const [signInInfo, setSignInInfo] = useState({
+    email: "",
+    password: "",
   });
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
@@ -43,150 +47,175 @@ const index = () => {
 
   useEffect(() => {
     const getRememberPassword = async () => {
-      const rememberPassword = await AsyncStorage.getItem('rememberPassword');
+      const rememberPassword = await AsyncStorage.getItem("rememberPassword");
       if (rememberPassword) {
         setRememberPassword(true);
       }
-    }
+    };
     getRememberPassword();
-  }, [])
+  }, []);
 
   const handleInputInfo = (key: string, value: string) => {
     setSignInInfo({
       ...signInInfo,
-      [key]: value
-    });
-  }
-
-  const handleRememberPassword = () => {
-    AsyncStorage.setItem('rememberPassword', JSON.stringify(!rememberPassword)); 
-    setRememberPassword(!rememberPassword); 
-  }
-
-  const handleLogin = () => {  
-    
-    axios.post(baseURL + '/signin', signInInfo) 
-    .then((response) => {
-      
-      console.log(response.data);
-      if(response.data.statusCode === 200) {
-        AsyncStorage.setItem('token', response.data.token);
-        AsyncStorage.setItem('user', JSON.stringify(response.data.user));
-        setIsLoggedIn(true);
-        navigation.navigate('(user)', { replace: true }); 
-      }
- 
-    })
-    .catch((error) => {
-      console.log(error);
+      [key]: value,
     });
   };
- 
+
+  const handleRememberPassword = () => {
+    AsyncStorage.setItem("rememberPassword", JSON.stringify(!rememberPassword));
+    setRememberPassword(!rememberPassword);
+  };
+
+  const handleLogin = () => {
+    axios
+      .post(baseURL + "/signin", signInInfo)
+      .then((response) => {
+        console.log(response.data);
+        if (response.data.statusCode === 200) {
+          AsyncStorage.setItem("token", response.data.token);
+          AsyncStorage.setItem("user", JSON.stringify(response.data.user));
+          setIsLoggedIn(true);
+          navigation.navigate("(user)", { replace: true });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   // return (
-    // <View style={{ flex: 1, justifyContent: 'center'}}>
-      {/* <Link href={'/(user)'} asChild>
+  // <View style={{ flex: 1, justifyContent: 'center'}}>
+  {
+    /* <Link href={'/(user)'} asChild>
         <Button text="User" />
-      </Link> */}
-      {/* <Link href={'/(admin)'} asChild>
+      </Link> */
+  }
+  {
+    /* <Link href={'/(admin)'} asChild>
         <Button text="Admin" />
       </Link> 
       <Link href={'/(authentication)'} asChild>
         <Button text="Authentication" />
-      </Link>  */}
-      {/* <View>Trang đăng nhập</View>
-    </View> */}
+      </Link>  */
+  }
+  {
+    /* <View>Trang đăng nhập</View>
+    </View> */
+  }
   // );
-  return (   
+  return (
     <View style={styles.divContainer}>
-      <Stack.Screen  options={{ headerShown: false }} />
+      <Stack.Screen options={{ headerShown: false }} />
       <ImageBackground
-        source={require('@assets/images/backgroundBlue.png')}
+        source={require("@assets/images/backgroundBlue.jpg")}
         style={{
           width: widthScreen,
           height: heightScreen,
           justifyContent: "center",
           alignItems: "center",
         }}
-      > 
-      <View style={{ marginBottom: heightScreen * 0.04}}>
-        <Text
+      >
+        <View style={{ marginBottom: heightScreen * 0.04 }}>
+          <Text
+            style={{
+              fontWeight: "bold",
+              fontSize: widthScreen * 0.07,
+              color: "white",
+            }}
+          >
+            ĐĂNG NHẬP
+          </Text>
+        </View>
+        <TextInput
+          style={styles.input}
+          placeholder="Email hoặc sđt"
+          onChangeText={(text) => handleInputInfo("email", text)}
+          value={signInInfo.email}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Mật khẩu"
+          onChangeText={(text) => handleInputInfo("password", text)}
+          value={signInInfo.password}
+        />
+        <View
           style={{
-            fontWeight: "bold",
-            fontSize: widthScreen * 0.07,
-            color: "white",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            width: widthScreen * 0.9,
+            marginVertical: heightScreen * 0.008,
           }}
-        >ĐĂNG NHẬP</Text>
-      </View>
-        <TextInput
-            style={styles.input}
-            placeholder="Email hoặc sđt"
-            onChangeText={text => handleInputInfo('email', text)}
-            value={signInInfo.email}
-        /> 
-        <TextInput
-            style={styles.input}
-            placeholder="Mật khẩu"
-            onChangeText={text => handleInputInfo('password', text)}
-            value={signInInfo.password}
-        /> 
-        <View style={{ flexDirection: 'row', justifyContent: "space-between", width: widthScreen * 0.9, marginVertical: heightScreen * 0.008 }}>
-          <View style={{ flexDirection: 'row', alignItems: "center" }}>
+        >
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
             <CheckBox
-                checked={rememberPassword}
-                onPress={() => handleRememberPassword()} 
-                containerStyle={{
-                    padding: 0,
-                    margin: 0,
-                    opacity: 1,
-                    borderColor: "white",
-                }}
-                checkedColor="white"
+              checked={rememberPassword}
+              onPress={() => handleRememberPassword()}
+              containerStyle={{
+                padding: 0,
+                margin: 0,
+                opacity: 1,
+                borderColor: "white",
+              }}
+              checkedColor="white"
             />
             <Text
-                style={{
-                    fontWeight: "bold",
-                    fontSize: widthScreen * 0.04,
-                    opacity: 1,
-                    alignSelf: "center",
-                    color: "white", 
-                    marginLeft: widthScreen * -0.02
-                }}
-            >Ghi nhớ mật khẩu</Text>
+              style={{
+                fontWeight: "bold",
+                fontSize: widthScreen * 0.04,
+                opacity: 1,
+                alignSelf: "center",
+                color: "white",
+                marginLeft: widthScreen * -0.02,
+              }}
+            >
+              Ghi nhớ mật khẩu
+            </Text>
           </View>
-          <View style={{flexDirection: 'row'}}>
+          <View style={{ flexDirection: "row" }}>
             <Text
-              style={{ textDecorationLine: 'underline', color: 'white', fontSize: widthScreen * 0.04}}
-            >Quên mật khẩu?</Text>
+              style={{
+                textDecorationLine: "underline",
+                color: "white",
+                fontSize: widthScreen * 0.04,
+              }}
+            >
+              Quên mật khẩu?
+            </Text>
           </View>
-        </View> 
-          <Button 
-            text='Đăng nhập' 
-            onPress={handleLogin}
-          >
-            <FontAwesome5 name="sign-in-alt" size={widthScreen * 0.03} color="white" />
-          </Button> 
-        <Link href={'/signUp'} asChild>
-          <View style={{flexDirection: 'row', alignItems: "center"}}>
+        </View>
+        <Button text="Đăng nhập" onPress={handleLogin}>
+          <FontAwesome5
+            name="sign-in-alt"
+            size={widthScreen * 0.03}
+            color="white"
+          />
+        </Button>
+        <Link href={"/signUp"} asChild>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
             <Text
               style={{
                 // fontWeight: "bold",
                 opacity: 0.8,
                 fontSize: widthScreen * 0.04,
                 color: "white",
-                textDecorationLine: 'underline',
+                textDecorationLine: "underline",
               }}
-              >Đăng ký tài khoản</Text>
-            <FontAwesome5 n
-              name="user-plus" 
+            >
+              Đăng ký tài khoản
+            </Text>
+            <FontAwesome5
+              n
+              name="user-plus"
               size={widthScreen * 0.03}
-              color="white" 
-              style={{ marginLeft: widthScreen * 0.01}}
-              />
+              color="white"
+              style={{ marginLeft: widthScreen * 0.01 }}
+            />
           </View>
-        </Link> 
+        </Link>
       </ImageBackground>
-    </View> 
-);
+    </View>
+  );
 };
 
 export default index;
